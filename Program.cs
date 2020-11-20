94162f823e1acd353d32086b1b4298c23a1dd3f8
sadia-github <72582600+sadia-github@users.noreply.github.com>
2020-11-19 20:18:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Finaltestcode
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Adult[] person = new Adult[] {new Adult(), new Child()};
+
+            foreach (Adult a in person)
+            {
+                a.Name();
+            }
+        }
+    }
+
+    public class Adult
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual void Name()
+        {
+            FirstName = "AdultFirstNAme";
+            LastName = "AdultLastNAme";
+            Console.WriteLine(FirstName);
+            Console.WriteLine(LastName);
+        }
+    }
+
+    class Child : Adult
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override void Name()
+        {
+            FirstName = "ChildFirstNAme";
+            LastName = "ChildLastNAme";
+            Console.WriteLine(FirstName);
+            Console.WriteLine(LastName);
+        }
+    }
+}
+
+
+
+
+
